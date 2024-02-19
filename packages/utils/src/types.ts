e8d7c7e55f515591644d1d7d70d808a462849c55
pierre <pierremarien29@gmail.com>
2024-02-19 15:47:34 +0100

    wip typage donnees

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,6 +1,19 @@
 export interface User {
-    name: string;
-    id: number;
-    mail: string;
-    password: string;
+  name: string;
+  id: number;
+  mail: string;
+  password: string;
+}
+// On a des projets de base commune en structure de donnees 
+
+export interface Projet {
+  id: number;
+  type: string;
+}
+
+export interface Urbanisme {
+  id: number;
+  type: string;
+  title: string;
+  description: string;
 }
