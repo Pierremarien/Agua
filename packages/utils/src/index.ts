bdba46b7d00fb07201cc87fb4ecb7bbb0042c943
pierre <pierremarien29@gmail.com>
2024-02-19 16:37:20 +0100

    premier jet typage des donnees

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,3 @@
-import { User } from "./types";
+import { Project, News, Reference, Company, Contact, Urbanisme, User, Architecture, Amenagement } from "./types";
 
-export type {User};
+export type { Project, News, Reference, Company, Contact, Urbanisme, User, Architecture, Amenagement };
--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,19 +1,75 @@
-export interface User {
-  name: string;
-  id: number;
-  mail: string;
-  password: string;
+export interface Project {
+  id: string;
+  type: "urbanisme" | "amenagement" | "architecture";
+  title: string;
+  description: string;
+  master: string;
+  status: string;
+  references: string[];
+  target: string;
+  images: string[];
+  extendedDescription: string;
+  date: Date;
 }
-// On a des projets de base commune en structure de donnees 
 
-export interface Projet {
-  id: number;
-  type: string;
+export interface News {
+  id: string;
+  title: string;
+  description: string;
+  date: Date;
+  images: string[];
+  linkedProject: string;
+}
+
+export interface Reference {
+  id: string;
+  title: string;
+  description: string;
+  images: string[];
+  linkedProject: string;
+  type: "public" | "private" | "press";
+}
+
+export interface Contact {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  position: string;
+  image: string;
+  description: string;
+  cv: string;
+}
+
+export interface Company {
+  id: string;
+  name: string;
+  address: string;
+  email: string;
+  phone: string;
+  description: string;
+  image: string;
+  logo: string;
+}
+
+export interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role: "admin" | "user" | "visitor";
+}
+
+export interface Architecture {
+  type: "publique" | "privé";
 }
 
 export interface Urbanisme {
-  id: number;
-  type: string;
-  title: string;
-  description: string;
+  type: "faisabilité" | "tourisme";
+}
+
+export interface Amenagement {
+  type: "abords" | "parc" | "place" | "equipment";
 }
