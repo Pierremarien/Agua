6e8cda998deb44fcc374c733b04b345e57d40625
antoinel74 <antoine.lansman@gmail.com>
2024-02-28 10:20:58 +0100

    fix semantic

--- packages/ui/src/components/list.tsx
+++ packages/ui/src/components/list.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Reference } from "@agua/utils";
 
 interface IList {
   references: Reference[];
+  isLarge: boolean;
 }
 
-export const List: React.FC<IList> = ({ references }) => {
+export const List: React.FC<IList> = ({ references, isLarge }) => {
   return (
     <ul>
       {references.map(({ title, link }, index) => (
-        <li key={index} title={title} className="w-auto py-1 text-2xl md:text-3xl">
+        <li
+          key={index}
+          title={title}
+          className={isLarge ? "w-auto py-1 text-3xl md:text-5xl" : "w-auto py-1 text-2xl md:text-3xl"}>
           <a href={link} className="opacity-60 hover:opacity-100">
             {title + " >"}
           </a>
