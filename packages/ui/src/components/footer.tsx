6713f1e399dde94948c22dd9ce454916dc73f14c
antoinel74 <antoine.lansman@gmail.com>
2024-02-29 14:39:12 +0100

    fix semantic

--- packages/ui/src/components/footer.tsx
+++ packages/ui/src/components/footer.tsx
@@ -14,7 +14,7 @@ export const Footer: React.FC<IFooter> = (props) => {
   const { home, address, phone, email, locality, name } = props;
 
   return (
-    <footer className={`w-full justify-between px-8 md:flex md:px-16 ${home ? "hidden" : ""}`}>
+    <footer className={`w-full justify-between px-8 md:flex md:gap-8 md:px-16 ${home ? "hidden" : ""}`}>
       {name && (
         <div>
           <h2 className="flex h-44 w-44 items-end  justify-center bg-[#231F20] p-4 text-6xl">{name}</h2>
@@ -23,8 +23,8 @@ export const Footer: React.FC<IFooter> = (props) => {
       {props && (
         <div className="space-y-8 text-2xl">
           <div className="py-4 md:py-0">
-            <h3>{address}</h3>
-            <h3> {locality}</h3>
+            <span className="block">{address}</span>
+            <span className="block"> {locality}</span>
           </div>
           <div>
             <a href={`tel:${phone}`} className="block underline underline-offset-2">
@@ -58,7 +58,7 @@ export const Footer: React.FC<IFooter> = (props) => {
               </a>
             </li>
           ))}
-          <li className="pt-12 font-light  opacity-60 md:pt-24">© 2024 Bibou Team. All rights reserved.</li>
+          <li className="pt-12 font-light  opacity-60 md:pt-24">© 2024 Bibou Team.</li>
         </ul>
       )}
     </footer>
