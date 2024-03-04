b57556725dc6d361cbc0d8b868a99f959ef808f0
antoinel74 <antoine.lansman@gmail.com>
2024-03-04 09:54:34 +0100

    feat/projectCard: add layout

--- packages/ui/src/components/ProjectCard.tsx
+++ packages/ui/src/components/ProjectCard.tsx
@@ -2,11 +2,23 @@ import React from "react";
 
 interface IProjectCard {
   preview: string;
-  img: any[];
+  img: string;
   client: string;
   title: string;
+  className: string;
 }
 
-export const ProjectCard: React.FC<IProjectCard> = ({ preview, img, client, title }) => {
-  return <div>ProjectCard</div>;
+export const ProjectCard: React.FC<IProjectCard> = ({ preview, img, client, title, className }) => {
+  if (!preview || !img || !client || !title || !className) {
+    return null;
+  }
+
+  return (
+    <div className={className + " relative"}>
+      <img src={img} alt={title} className="absolute h-full w-full" />
+      <h3>{title}</h3>
+      <span className="block">{client}</span>
+      <p>{preview}</p>
+    </div>
+  );
 };
