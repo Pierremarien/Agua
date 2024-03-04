3bac055944b0ab52ed806d0e1cb317183c977255
antoinel74 <antoine.lansman@gmail.com>
2024-03-04 09:18:31 +0100

    feat/list: state between nested and normal list done

--- packages/ui/src/components/list.tsx
+++ packages/ui/src/components/list.tsx
@@ -4,28 +4,44 @@ import { Reference, Client } from "@agua/utils";
 interface IList {
   references: Reference[];
   hideChevron: boolean;
-  clients: Client[];
+  clients?: Client[];
 }
 
 export const List: React.FC<IList> = ({ references, hideChevron, clients }) => {
+  console.log(references);
   return (
-    <ul>
-      {clients.map(({ name, id }) => (
-        <li key={id} title={name} className="group flex gap-4 py-2 text-3xl md:text-4xl">
-          <span className="opacity-60 hover:opacity-100 group-hover:opacity-100">{name + " >"}</span>
-          {references && (
-            <ul className="hidden group-hover:block">
-              {references.map(({ title, link }, index) => (
-                <li key={index} title={title} className="w-auto py-1 text-2xl md:text-3xl">
-                  <a href={link} className="opacity-60 hover:opacity-100">
-                    {!hideChevron ? title + " >" : title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          )}
-        </li>
-      ))}
-    </ul>
+    <div>
+      {clients && (
+        <ul>
+          {clients.map(({ name, id }) => (
+            <li key={id} title={name} className="group flex gap-4 py-2 text-3xl md:text-4xl">
+              <span className="opacity-60 hover:opacity-100 group-hover:opacity-100">{name + " >"}</span>
+              {references && (
+                <ul className="hidden group-hover:block">
+                  {references.map(({ title, link }, index) => (
+                    <li key={index} title={title} className="w-auto py-1 text-2xl md:text-3xl">
+                      <a href={link} className="opacity-60 hover:opacity-100">
+                        {!hideChevron ? title + " >" : title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
+      {!clients && (
+        <ul>
+          {references.map(({ title, link }, index) => (
+            <li key={index} title={title} className="w-auto py-1 text-2xl md:text-3xl">
+              <a href={link} className="opacity-60 hover:opacity-100">
+                {!hideChevron ? title + " >" : title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
